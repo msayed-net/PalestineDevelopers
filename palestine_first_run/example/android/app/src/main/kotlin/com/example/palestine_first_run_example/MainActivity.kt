8e761093fd3b6c0763e177f2c58113505c862a11
Mohamed Sayed <creative.mohamed.sayed@gmail.com>
2021-12-27 12:21:01 +0200

    dev - maintain project commands

--- palestine_first_run/example/android/app/src/main/kotlin/com/example/palestine_first_run_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.palestine_first_run_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- palestine_trusted_device/example/android/app/src/main/kotlin/net/msayed/palestine_trusted_device_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package net.msayed.palestine_trusted_device_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
